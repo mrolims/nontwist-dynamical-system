5d68f22cf42cf6f49a39cf60fa91fed04fe2e745
Matheus Rolim Sales <matheusrolim95@gmail.com>
2024-05-08 16:59:37 -0300

    Update the user input parameters

--- lyapunov_vs_eps.py
+++ lyapunov_vs_eps.py
@@ -6,7 +6,9 @@ Calculates the largest Lyapunov exponent as a function of eps.
 
 Usage
 -----
-        python lyapunov_vs_eps.py
+This script takes on two parameters
+
+        python lyapunov_vs_eps.py n N
 
 Author: Matheus Rolim Sales
 Email: matheusrolim95@gmail.com
@@ -15,15 +17,16 @@ Last updated: 08/05/2024
 import numpy as np
 from functions import *
 from joblib import Parallel, delayed
+import sys
 import os
 # The path variable defines the path to where the data will be stored.
 path = "Data"
 if not os.path.isdir(path):
     os.system("mkdir %s" % path)
 
-# --- Parameters of the simulation --- #
-n = 1000 # Number of eps samples
-N = int(1e3) # Maximum iteration time
+# --- User input --- #
+n = int(sys.argv[1]) # Number of eps samples
+N = int(float(sys.argv[2])) # Maximum iteration time
 # Initial condition
 psi = 0.5
 I = 1e-10
